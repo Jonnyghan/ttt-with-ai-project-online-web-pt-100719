d1ba8b9dc865e61e32984c96fa5c9cb1056659c7
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-12-05 21:07:48 +0000

    Automatically backed up by Learn

---
+++ lib/game.rb
@@ -0,0 +1,14 @@
+class Game
+  
+   WIN_COMBINATIONS = [
+    [0,1,2], #Top Row 
+    [3,4,5], #Middle Row 
+    [6,7,8], #Bottom Row
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+    ]
+  
+end
